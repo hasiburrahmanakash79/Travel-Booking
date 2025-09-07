d1c3486afa4d6ab2973f59d5cfadeb81be66966a
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-09-07 10:19:44 +0600

    Implement code changes to enhance functionality and improve performance

--- src/Pages/Authentication/SignIn.jsx
+++ src/Pages/Authentication/SignIn.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { FaUser } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import authImage from "../../assets/images/authImage.jpg"
 
 const SignIn = () => {
   const {
@@ -22,33 +23,36 @@ const SignIn = () => {
   };
 
   return (
-    <div className="grid grid-cols-7 min-h-screen bg-base-200">
+    <div className="grid grid-cols-8 min-h-screen bg-base-200">
       {/* Left Side */}
-      <div className="col-span-3 bg-blue-500 flex items-center p-8">
-        <h2 className="text-white text-4xl font-bold leading-relaxed">
-          Welcome Back! Verify Your <br /> Email to Access Your <br /> Learning
-          Portal!
+      <div
+        className="col-span-3 flex flex-col justify-start items-center bg-cover bg-center bg-no-repeat p-8 relative"
+        style={{ backgroundImage: `url(${authImage})` }}
+      >
+        <div className="absolute inset-0 bg-black/50"></div>
+        <h2 className="text-white text-4xl font-medium leading-relaxed relative z-10 text-center mt-16">
+          Book your Hotel
         </h2>
+        <p className="text-white text-sm text-center relative z-10">
+          More than 50,000 hotels nationwide
+        </p>
       </div>
 
       {/* Right Side */}
-      <div className="col-span-4 flex items-center justify-center ">
-        <div className="max-w-lg w-full bg-white rounded-3xl border border-blue-200 shadow-md p-16">
-          <h2 className="text-2xl font-bold text-center mb-2">
-            Sign in Account
+      <div className="col-span-5 flex items-center justify-center ">
+        <div className="max-w-sm w-full bg-white rounded-3xl  shadow-md p-10">
+          <h2 className="text-2xl font-medium mb-2">
+            Sign in & Explore
           </h2>
-          <p className="text-center text-sm mb-6">
-            Don’t have an Account?{" "}
-            <Link to='/signup' className="text-blue-500 hover:underline">
-              Sign Up Free
-            </Link>
+          <p className="text-xs mb-6">
+            Log into your account with your email, or create one below.
           </p>
 
           {/* Form */}
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div>
               <label className="block text-sm font-medium mb-1">
-                User name
+                Email or phone number
               </label>
               <div className="relative">
                 <input
@@ -56,8 +60,8 @@ const SignIn = () => {
                   {...register("username", {
                     required: "Username is required",
                   })}
-                  placeholder="Enter your user name"
-                  className="w-full border border-base-300 bg-base-200 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Email or phone number"
+                  className="w-full border border-base-300 rounded-md p-2 outline-none"
                 />
                 <FaUser className="absolute inset-y-3 right-3 flex items-center text-gray-500" />
               </div>
@@ -77,7 +81,7 @@ const SignIn = () => {
                     required: "Password is required",
                   })}
                   placeholder="Enter your Password"
-                  className="w-full border border-base-300 bg-base-200 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="w-full border border-base-300 rounded-md p-2 outline-none"
                 />
                 <button
                   type="button"
@@ -100,24 +104,24 @@ const SignIn = () => {
                     {...register("remember")}
                     className="mr-2"
                   />
-                  Remember for 30 Days
+                  Remember me
                 </div>
-                <a href="#" className="text-blue-500 hover:underline">
+                {/* <a href="#" className="text-blue-500 hover:underline">
                   Forgot Password?
-                </a>
+                </a> */}
               </div>
             </div>
 
             <button
               type="submit"
-              className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md"
+              className="w-full bg-[#7ED957] hover:shadow-lg transform duration-300 text-black font-medium py-2 rounded-md"
             >
               Login
             </button>
           </form>
 
           {/* Divider */}
-          <div className="divider">Or Login with</div>
+          <div className="divider">Or </div>
           {/* Social Login */}
           <div className="flex space-x-4">
             <button className="flex-1 flex items-center justify-center border border-base-300 rounded-md py-2 hover:bg-gray-100">
@@ -126,7 +130,6 @@ const SignIn = () => {
                 alt="Google"
                 className="w-5 h-5 mr-2"
               />
-              Google
             </button>
             <button className="flex-1 flex items-center justify-center border border-base-300 rounded-md py-2 hover:bg-gray-100">
               <img
@@ -134,15 +137,14 @@ const SignIn = () => {
                 alt="Facebook"
                 className="w-5 h-5 mr-2"
               />
-              Facebook
             </button>
           </div>
 
           {/* Bottom Signup */}
           <p className="text-center text-sm mt-6">
             Don’t have an account?{" "}
-            <Link to='/signup' className="text-blue-500 hover:underline">
-              Sign Up Free
+            <Link to='/signup' className="text-[#25D366] hover:underline">
+              Create an account
             </Link>
           </p>
         </div>
