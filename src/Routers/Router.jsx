97b695ae564f0a9ae0f7ec0806c3578aa963303a
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-09-10 14:12:02 +0600

    new routes added

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -3,6 +3,11 @@ import Signup from "../Pages/Authentication/Signup";
 import SignIn from "../Pages/Authentication/SignIn";
 import Main from "../Layouts/Main";
 import Homepage from "../Pages/HomePage/Homepage";
+import HotelRequest from "../Pages/HotelRequest/HotelRequest";
+import Offers from "../Pages/Offers/Offers";
+import AboutUs from "../Pages/AboutUs/AboutUs";
+import Faq from "../Pages/Faq/Faq";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -10,13 +15,27 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Homepage/>
+        element: <Homepage />,
       },
-      
-
+      {
+        path: "/hotel-request",
+        element: <HotelRequest />,
+      },
+      {
+        path: "/offers",
+        element: <Offers />,
+      },
+      {
+        path: "/about-us",
+        element: <AboutUs />,
+      },
+      {
+        path: "/faq",
+        element: <Faq />,
+      },
     ],
   },
-  
+
   {
     path: "/signin",
     element: <SignIn />,
